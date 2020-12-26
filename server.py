e44477f6857ff56cccdaabd9ebca267f03ad7355
Bassel A. Elerian <basselakmal@gmail.com>
2020-12-26 14:31:07 +0200

    Telegram Bot - P07 - Getting Free SSL Certificate

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask
 
 PORT = 8080
+CERT_PATH = '/home/bassel/telegram_bot/ssl/fullchain.pem'
+KEY_PATH = '/home/bassel/telegram_bot/ssl/privkey.pem'
 
 app = Flask(__name__)
 
@@ -8,5 +10,10 @@ app = Flask(__name__)
 def main(*args, **kwargs):
     return "Welcome to our server!"
 
+@app.route('/.well-known/acme-challenge/<challenge>')
+def verify_challenge(challenge):
+    challenge_file = open('.well-known/acme-challenge/' + challenge)
+    return challenge_file.read()
+
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=PORT)
+    app.run('0.0.0.0', port=PORT, ssl_context=(CERT_PATH, KEY_PATH))
